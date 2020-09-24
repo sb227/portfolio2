ba9158b00e34dd7d0ab9f641dc88731ea24277a2
sb227 <69783498+sb227@users.noreply.github.com>
2020-09-24 17:57:57 +0900

    200924

--- js/main.js
+++ js/main.js
@@ -1,72 +1,113 @@
 /// 메인 페이지 JS - main.js ////
 
-$(function(){ /////// jQB ///////////////////////
+$(function () { /////// jQB ///////////////////////
     console.log("로딩완료!");
     
+    $("#wrap").smoothWheel()
+
     // 아이템 위치 변수
     var itpos = [];
     // 클래스 itpos를 준 요소의 개수만큼 위치를 알아온다!
     var itcnt = $(".itpos").length;
-    console.log("위치개수:"+itcnt);
-    for(var i=0; i< itcnt; i++){
+    console.log("위치개수:" + itcnt);
+    for (var i = 0; i < itcnt; i++) {
         itpos[i] = $(".itpos").eq(i).offset().top;
     } ////// for문 /////////////////
-    
-    console.log("위치순서:"+itpos);
-    
-    
+
+    console.log("위치순서:" + itpos);
+
+
     // 위치갭(이전,이후)
     var gap = 200;
     ////// scroll 셋팅 ///////////
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         var scTop = $(this).scrollTop();
-        console.log("스위:"+scTop);
-        
-        
+        console.log("스위:" + scTop);
+
         // 1번째 기준선 액션 ///
-        if(scTop > itpos[0]-gap*2 && 
-           scTop < itpos[0]+gap*2){
+        if (scTop > itpos[0] - gap * 2 &&
+            scTop < itpos[0] + gap * 2) {
             // class넣기
             $("#sun_cont_wrap .title_wrap").addClass("on");
         } ///////// if ////////////////////
-        else{
+        else {
             $("#sun_cont_wrap .title_wrap").removeClass("on");
         } ///////// else ////////////////////
-        
+
         // 2번째 기준선 액션 ///
-        if(scTop > itpos[1]-gap && 
-           scTop < itpos[1]+gap){
+        if (scTop > itpos[1] - gap &&
+            scTop < itpos[1] + gap) {
             // class넣기
             $(".sun_cont1").addClass("on");
         } ///////// if ////////////////////
-        else{
+        else {
             $(".sun_cont1").removeClass("on");
         } ///////// else ////////////////////
-        
-        
+
+
         // 3번째 기준선 액션 ///
-        if(scTop > itpos[2]-gap && 
-           scTop < itpos[2]+gap){
+        if (scTop > itpos[2] - gap &&
+            scTop < itpos[2] + gap) {
             // class넣기
             $(".sun_cont2").addClass("on");
         } ///////// if ////////////////////
-        else{
+        else {
             $(".sun_cont2").removeClass("on");
         } ///////// else ////////////////////
-        
-    });///////// scroll //////////////
-    
-    
-    
+
+        // 4번째 기준선 액션 ///
+        if (scTop > itpos[3] - gap * 2 &&
+            scTop < itpos[3] + gap * 2) {
+            // class넣기
+            $("#gls_cont_wrap .title_wrap").addClass("on");
+        } ///////// if ////////////////////
+        else {
+            $("#gls_cont_wrap .title_wrap").removeClass("on");
+        } ///////// else ////////////////////
+
+        // 5번째 기준선 액션 ///
+        if (scTop > itpos[4] - gap * 2 &&
+            scTop < itpos[4] + gap * 2) {
+            // class넣기
+            $("#shop_wrap .title_wrap").addClass("on");
+        } ///////// if ////////////////////
+        else {
+            $("#shop_wrap .title_wrap").removeClass("on");
+        } ///////// else ////////////////////
+    }); ///////// scroll //////////////
+
     /// 셋팅된 위치에서 등장액션 class주기
-    
-    
-    
-    
-    
-    
-    
-    
-    
-});////////// jQB ///////////////////////////////
+
+    // 메뉴 li 오버시 배경 바뀌게
+    $(".nav_list li").mouseover(function () {
+        var i = $(this).index();
+        // console.log(i);
+
+        $("#menu_wrap").css({
+            backgroundImage: "url(images/menu" + (i + 1) + ".jpg)"
+        })
+    }); ////// mouseover //////////
+
+    // 햄버거 메뉴 on 넣기
+    $(".menu_btn").click(function () {
+        $("#menu_wrap").toggleClass("on");
+    }); ////////// click /////////////
+
+
+    var move = $(".gls_list");
+
+    move.draggable({
+            axis: "x" // 축고정("x"는 x축고정, "y"는 y축고정)
+        })
+        // 트랜지션 설정
+        .css({
+            transition: "all .4s ease-out"
+        });
+
+
+
+
+
+
+}); ////////// jQB ///////////////////////////////
 /////////////////////////////////////////////////
