b7161e69ce5d733b53dd34e180b49494680a9982
sb227 <69783498+sb227@users.noreply.github.com>
2020-10-05 14:34:23 +0900

    201005

--- js/gallery.js
+++ js/gallery.js
@@ -12,49 +12,41 @@ var psts = 0; //(0-허용,1-불허용)
 
 
 $(function () { ////////////////// JQB ////////////
-console.log("로딩완료");
+    console.log("로딩완료");
 
-totsize = $("#photo").width() - $(window).width();
+    totsize = $("#photo").width() - $(window).width();
 
-$(document).on("mousewheel DOMMouseScroll",
-function (e) {
-    // console.log("휠");
+    $(document).on("mousewheel DOMMouseScroll",
+        function (e) {
+            // console.log("휠");
 
-    e = window.event || e;
-    var delta = e.detail ? e.detail : e.wheelDelta;
-    if (/Firefox/i.test(navigator.userAgent)) {
-        //console.log("난파폭!");
-        delta = -delta; //부호를 반대로!
-    } ////// if문 ////////////////////////
+            e = window.event || e;
+            var delta = e.detail ? e.detail : e.wheelDelta;
+            if (/Firefox/i.test(navigator.userAgent)) {
+                //console.log("난파폭!");
+                delta = -delta; //부호를 반대로!
+            } ////// if문 ////////////////////////
 
-    console.log(delta);
-    if (delta < 0) {
-        scno += 120;
-        if (scno > totsize) scno = totsize;
-    } else {
-        scno -= 120;
-        if (scno < 0) scno = 0;
-    } /// else //////
+            console.log(delta);
+            if (delta < 0) {
+                scno += 120;
+                if (scno > totsize) scno = totsize;
+            } else {
+                scno -= 120;
+                if (scno < 0) scno = 0;
+            } /// else //////
 
-    $("#wrap").stop().animate({
-        scrollLeft: scno + "px"
-}, 2000, "easeOutQuint");
+            $("#wrap").stop().animate({
+                scrollLeft: scno + "px"
+            }, 2000, "easeOutQuint");
 
-}); ///////////////// 마우스 휠 ////////////
-
-
-// 이미지 뿌리기
-for (var i = 1; i < 13; i++) {
-    $("#photo").append("<li class='galimg"+i+"'><img src='images/gallery/" + i + ".jpg'></li>");
-//
-//    $("#photo li:nth-child(" + i + ")").css({
-//        top: (Math.random()*40) + "%",
-//        left: (Math.random()*90) + "%"
-//    }); //// css ///////////
+        }); ///////////////// 마우스 휠 ////////////
 
 
-} ////// for문 ////////
-
+    // 이미지 뿌리기
+    for (var i = 1; i < 13; i++) {
+        $("#photo").append("<li class='galimg" + i + "'><a href='images/gallery/" + i + ".jpg' data-lightbox='view'><img src='images/gallery/" + i + ".jpg'></a></li>");
+    } ////// for문 ////////
 
 
 }); ////////////////// JQB //////////////////////
--- js/show.js
+++ js/show.js
@@ -1,22 +1,51 @@
-$(function(){ ////////////////// JQB ////////////
+$(function () { ////////////////// JQB ////////////
     console.log("로딩완료");
-        
-    for(var i=1;i<6;i++){
-        $(".show_list").append("<li><img src='images/show/hendrix_mandarincrystal_dt_0"+i+".jpg'></li>")
-    } ////////// for /////////////////
     
+    // $("#wrap").smoothWheel();
+    
+    // 왼쪽 이미지 넣기
+    for (var i = 1; i < 6; i++) {
+        $(".show_list").append("<li><img src='images/show/hendrix_mandarincrystal_dt_0" + i + ".jpg'></li>")
+    } ////////// for /////////////////
+
+    // 상세 정보 클릭시
     $(".show_info div").hide();
-    $(".show_info li").click(function(){
+    $(".show_info li").click(function () {
         $(this).find("div").slideToggle();
         $(this).css({
-            height:"100%"
+            height: "100%"
         }); /////// css ////////
         $(this).toggleClass("on");
     }); //////////// click /////////////////////
+
+    // 하트 토글
+    $(".heart").click(function(){
+        $(this).toggleClass("on");
+    }); ///// click //////////   
     
     
-    
-    
+    // 위치갭(이전,이후)
+    var gap = 200;
+
+    ////// scroll 셋팅 ///////////
+    $("#wrap").scroll(function () {
+        var scTop = $(this).scrollTop();
+        console.log("스위:" + scTop);
+
+        // 왼쪽 높이 구하기
+        var lpos = $(".show_list").height();
+        console.log(lpos);
+        
+        // 로고 기준선 액션 ///
+        if (scTop > lpos - gap * 2) {
+            console.log("하이하이");
+            // top 값이 움직이게
+        } /////// if ///////////
+
+    });
+
+
+
 }); ////////////////////////////////////////////
 ///////////////////JQB  ////////////////////////
 ///////////////////////////////////////////////
