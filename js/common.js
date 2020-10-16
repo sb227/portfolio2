6ccfbfb267b06b55da298a5a62434159e4fb715a
sb227 <69783498+sb227@users.noreply.github.com>
2020-10-16 17:24:41 +0900

    201016

--- js/common.js
+++ js/common.js
@@ -8,11 +8,37 @@ $(function () { ////////////////// JQB ////////////
             $("#header").toggleClass("on");
     }); ////////// click /////////////
     
-    /// event 메뉴 클릭시
-    $(".nav_list li").last().click(function(){
-        alert("해당 페이지는 준비중입니다.");
-    }); /////// click /////////
 
+    // 로케이션 설정
+
+        // 로고
+        $(".logo").click(function(){
+            location.href="index.html";
+        });/////// click /////////
+        
+        // 메뉴
+        $(".nav_list").find("a").first().click(function(){
+            location.href="gallery.html";
+        });/////// click /////////
+
+         $(".nav_list li:nth-child(2) a, .nav_list li:nth-child(3) a").click(function(){
+            location.href="list.html"
+        });/////// click /////////
+
+        $(".nav_list").find("a").last().click(function(){
+            alert("해당 페이지는 준비중입니다.");
+        }); /////// click /////////
+    
+        // 선글라스 리스트 
+        $(".cont_img").click(function(){
+            location.href="list.html";
+        }); /////// click /////////
+        
+        // 안경 show
+        $(".gls_list a").click(function(){
+            location.href="show.html";
+        }); /////// click /////////
+    
     // 메뉴 li 오버시 배경 바뀌게
     $(".nav_list li").mouseover(function () {
         var i = $(this).index();
@@ -22,7 +48,8 @@ $(function () { ////////////////// JQB ////////////
             background: "url(images/menu" + (i + 1) + ".jpg) no-repeat center/cover"
         })
     }); ////// mouseover //////////
-
+    
+    
 
 }); ////////////////// JQB //////////////////////
 /////////////////////////////////////////////////
